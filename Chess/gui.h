0183d459442fe7c2f47f5b45b9a26dfef1f0dae0
nirfun <nir123@gmail.com>
2015-09-26 11:41:10 +0300

    code cosmetics

--- Chess/gui.h
+++ Chess/gui.h
@@ -2,6 +2,9 @@
 #define GUI_H
 #include "SDL.h"
 #include "fileHandle.h"
+#define WIN_TITLE "Chess"
+#define WIN_HEIGHT 600
+#define WIN_WIDTH 800
 
 typedef struct ImgButton ImgButton;
 int isButtonClicked(ImgButton btn, int clickedX, int clickedY);
