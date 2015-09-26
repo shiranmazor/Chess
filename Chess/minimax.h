0f014ee41049b597415cc66a976ae0c4f36be294
shiranmazor <shiran.mazor@gmail.com>
2015-09-26 20:41:41 +0300

    Revert "fix compile errror"
    This reverts commit d3a9d8a4568f028c744a5185510bcc73b9b97d71.

--- Chess/minimax.h
+++ Chess/minimax.h
@@ -10,7 +10,7 @@
 #include "ChessHelp.h"
 #endif CHESS_HELP_H
 
-
+int maxBoardNum = 1000000;
 int minimax(char board[BOARD_SIZE][BOARD_SIZE], int depth, int computerColor,
 	Move** bestMove, int alpha, int beta, int isMax, int boardCounter);
 int score(char board[BOARD_SIZE][BOARD_SIZE], int PlayerColor);
