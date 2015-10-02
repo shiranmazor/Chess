d756ce5ab280d3b367ab2c1228035e779ddd0ef0
shiranmazor <shiran.mazor@gmail.com>
2015-10-02 17:29:18 +0300

    chessprog

---
+++ Chess/chessprog.c
@@ -0,0 +1,10 @@
+#include "Console.h"
+#include "gui.h"
+
+int main(int argc, char* args[])
+{
+	if (strcmp(args[1], "console") == 0)
+		runConsole();
+	else if (strcmp(args[1], "gui") == 0)
+		RunGui();
+}
